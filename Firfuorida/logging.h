0280a49e22c861e64660a9e9617a47e0e35162de
buschmann23 <mf@huessenbergnetz.de>
2022-01-14 19:20:53 +0100

    Add logging.h for logging categories

---
+++ Firfuorida/logging.h
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2019-2021 Matthias Fehring / www.huessenbergnetz.de
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <QLoggingCategory>
+
+// Q_LOGGING_CATEGORY is defined in migrator.cpp
+Q_DECLARE_LOGGING_CATEGORY(FIR_CORE)
+
+#endif // LOGGING_H
