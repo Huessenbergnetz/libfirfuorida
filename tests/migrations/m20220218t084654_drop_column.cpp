7094adea8bff8da8388aeb58a0cc60eb1a50d14c
Buschmann <mf@huessenbergnetz.de>
2022-09-22 14:20:29 +0200

    Try to fix DROP COLUMN test on old SQLite
    SQLite below 3.35.0 does not support ALTER TABLE DROP COLUMN command.

--- tests/migrations/m20220218t084654_drop_column.cpp
+++ tests/migrations/m20220218t084654_drop_column.cpp
@@ -19,8 +19,41 @@ void M20220218T084654_Drop_column::up()
 
 void M20220218T084654_Drop_column::down()
 {
-    auto t = table(QStringLiteral("tiny"));
-    t->dropColumn(QStringLiteral("colToDrop"));
+    if (dbType() == Firfuorida::Migrator::SQLite && dbVersion() < QVersionNumber(3, 35, 0)) {
+        // SQLite below version 3.35.0 does not support ALTER TABLE DROP COLUMN
+        raw(QStringLiteral("BEGIN TRANSACTION"));
+
+        // create temporary table without column to drop
+        auto temp = create(QStringLiteral("tiny_backup"));
+        temp->setIsTemporary(true);
+        temp->tinyIncrements();
+        temp->tinyInteger(QStringLiteral("tinyIntCol"))->unique();
+        temp->tinyBlob(QStringLiteral("tinyBlobCol"))->nullable();
+        temp->tinyText(QStringLiteral("tinyTextCol"))->defaultValue(QStringLiteral("dummer schiss"));
+
+        // copy current data from original table to backup table
+        raw(QStringLiteral("INSERT INTO tiny_backup SELECT id, tinyIntCol, tinyBlobCol, tinyTextCol FROM tiny"));
+
+        // drop original table
+        drop(QStringLiteral("tiny"));
+
+        auto nt = create(QStringLiteral("tiny"));
+        nt->tinyIncrements();
+        nt->tinyInteger(QStringLiteral("tinyIntCol"))->unique();
+        nt->tinyBlob(QStringLiteral("tinyBlobCol"))->nullable();
+        nt->tinyText(QStringLiteral("tinyTextCol"))->defaultValue(QStringLiteral("dummer schiss"));
+
+        // copy backup data into new table
+        raw(QStringLiteral("INSERT INTO tiny SELECT id, tinyIntCol, tinyBlobCol, tinyTextCol FROM tiny_backup"));
+
+        // drop backup table
+        drop(QStringLiteral("tiny_backup"));
+
+        raw(QStringLiteral("COMMIT"));
+    } else {
+        auto t = table(QStringLiteral("tiny"));
+        t->dropColumn(QStringLiteral("colToDrop"));
+    }
 }
 
 #include "moc_m20220218t084654_drop_column.cpp"
